5e24ebf57043c11b09da2da39fd8a1227853e853
omidrobotic <51260044+omidrobotic@users.noreply.github.com>
2021-06-22 11:54:38 +0430

    74

--- omid/ai/Referee.cpp
+++ omid/ai/Referee.cpp
@@ -62,25 +62,25 @@ void Refree::Refree_parser(World &world)
         if (World::team_color==TC_Yellow)
         {
             world.getInstance().team_side=TS_LeftSide;
-            world.setTeamSide(TS_LeftSide);
+            world.setTeamSide(TS_RightSide);
         }
         else
         {
             world.getInstance().team_side= TS_RightSide;
-            world.setTeamSide(TS_RightSide);
+            world.setTeamSide(TS_LeftSide);
         }
     }
     else
     {
         if (World::team_color==TC_Blue)
         {
-            world.getInstance().team_side=TS_RightSide;
-            world.setTeamSide(TS_RightSide);
+            world.getInstance().team_side=TS_LeftSide;
+            world.setTeamSide(TS_LeftSide);
         }
         else
         {
-            world.getInstance().team_side= TS_LeftSide;
-            world.setTeamSide(TS_LeftSide);
+            world.getInstance().team_side= TS_RightSide;
+            world.setTeamSide(TS_RightSide);
         }
     }
 
